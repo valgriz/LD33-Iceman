fd48ef63260bdfe7bf3bdb92872d5233da111819
Steven Marturano <steven.marturano@gmail.com>
2015-08-22 19:44:53 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var game = new Phaser.Game(820, 560, Phaser.AUTO, '',{preload: preload, create: create, update: update});
+
+this.game.stage.scape.pageAlignHorizontally = true;
+this.game.stage.scale.pageAlighVertically = true;
+this.game.stage.scale.refresh();
+
+function preload(){
+
+}
+
+function create(){
+
+}
+
+function update(){
+
+}
