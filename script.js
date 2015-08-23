ae8ffcabad92fc0fe0a2d4c837ffdb805118486e
Steven Marturano <steven.marturano@gmail.com>
2015-08-23 14:42:22 -0400

    added thermometer

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var gFire
 //visible objects
 var monster;
 var fire;
+var liquid;
 
 //animations
 var flame;
@@ -22,6 +23,11 @@ var dKey;
 var wKey;
 var spaceBar;
 
+//variables
+var logicTimer;
+var temperature;
+var gameEnded;
+
 this.game.stage.scape.pageAlignHorizontally = true;
 this.game.stage.scale.pageAlighVertically = true;
 this.game.stage.scale.refresh();
@@ -31,6 +37,8 @@ function preload(){
 	game.load.image('bgd_base','assets/images/bgd_base.png');
 	game.load.image('wall','assets/images/wall_a1.png');
 	game.load.image('monster','assets/images/monster_a1.png');
+	game.load.image('therm_base','assets/images/therm_base.png');
+	game.load.image('therm_liquid','assets/images/therm_liquid.png');
 
 	game.load.spritesheet('fire', 'assets/images/fire_a1.png', 64, 64, 4);
 	game.load.spritesheet('monsterSpriteSheet', 'assets/images/monstersheet_a1.png', 41, 64, 16);
@@ -61,7 +69,8 @@ function create(){
 	gIceBlockEnvironment = new Phaser.Group(game);
 	gFire = new Phaser.Group(game);
 
-	
+	logicTimer = 250;
+	temperature = 0;
 
 	for(var i = 0; i < 14; i++){
 		gIceBlockBorder.add(game.add.sprite((i * 64), 560 - (1 * 64), 'wall'));
@@ -71,13 +80,20 @@ function create(){
 	game.physics.enable([monster, gIceBlockBorder], Phaser.Physics.ARCADE);
 	monster.body.gravity.y = 1400;
 	gIceBlockBorder.forEach(function(sp){
-		sp.body.velocity.x = -100;
+		sp.body.velocity.x = -100 * (logicTimer / 250);
 	    sp.body.immovable = true;
 	}, this);
 
 
+	liquid = game.add.sprite(59, 35, 'therm_liquid');
+	game.add.sprite(25, 25, 'therm_base');
+	
+	liquid.scale.x = (temperature/100);
+
+	gameEnded = false;
 
 }
+
 
 function spawnFlame(){
 	var tempFire = game.add.sprite(832, 560 - ((1 + 1) * 64),'fire');
@@ -88,7 +104,7 @@ function spawnFlame(){
 
 	game.physics.enable([gFire], Phaser.Physics.ARCADE);
 	gFire.forEach(function(ds){
-					ds.body.velocity.x = -100;
+					ds.body.velocity.x = -100 * (logicTimer / 250);
 			   		ds.body.immovable = true;
 				}, this);
 			}
@@ -97,7 +113,7 @@ function spawnEnvironmentBlock(height){
 	gIceBlockEnvironment.add(game.add.sprite(832,  560 - ((height + 1) * 64), 'wall'));
 	game.physics.enable([gIceBlockEnvironment], Phaser.Physics.ARCADE);
 	gIceBlockEnvironment.forEach(function(ds){
-					ds.body.velocity.x = -100;
+					ds.body.velocity.x = -100 * (logicTimer / 250);
 			   		ds.body.immovable = true;
 				}, this);
 			var rnd = Math.random();
@@ -112,12 +128,50 @@ function removeElements(arrayOfElementsToRemove){
 	}
 }
 
+function gameOver(){
+	alert('GAME OVER!!!\nThanks for playing!');
+	gameEnded = true;
+}
+
+function monsterFireCollide(){
+	if(temperature<100){
+		temperature++;
+	} else {
+		if(!gameEnded){
+			gameOver();
+		}
+	}
+}
+
+
 function update(){
 
+	var remEle = [];
+
+	gIceBlockEnvironment.forEach(function(sp){
+		sp.body.velocity.x = -100 * (logicTimer / 250);
+		if(sp.body.x <= -64){
+				sp.body.x = 100;
+				sp.body.y = 100;
+				remEle.push(sp);
+		}
+	}, this);
+
+	gFire.forEach(function(sp){
+		sp.body.velocity.x = -100 * (logicTimer / 250);
+		//game.physics.arcade.collide(monster, sp, monsterFireCollide, null, this);
+		if((monster.body.x + 41 > sp.body.x) && (monster.body.x < sp.body.x + 64 )){
+			if((monster.body.y + 41 > sp.body.y) && (monster.body.y < sp.body.y + 64)){
+			monsterFireCollide();
+			}
+		}
+	}, this);
 
 	gIceBlockBorder.forEach(function(sp){
+		sp.body.velocity.x = -100 * (logicTimer / 250);
 		if(sp.body.x <= -64){
 			sp.body.x = 832;
+			logicTimer++;
 			var rnd = Math.random();
 			if(rnd < .6){
 				spawnEnvironmentBlock(1);
@@ -128,20 +182,15 @@ function update(){
 		}
 	}, this);
 
-	var remEle = [];
+	liquid.scale.x = (temperature/100);
 
-	gIceBlockEnvironment.forEach(function(sp){
-		if(sp.body.x <= -64){
-				sp.body.x = 100;
-				sp.body.y = 100;
-				remEle.push(sp);
-		}
-	}, this);
 
 	removeElements(remEle);
 
 	game.physics.arcade.collide(monster, gIceBlockBorder);
 	game.physics.arcade.collide(monster, gIceBlockEnvironment);
+
+
 
 
 	if(leftKey.isDown || aKey.isDown){
@@ -161,7 +210,8 @@ function update(){
  		}
  	}
 
- 	if(upKey.isDown || wKey.isDown){
+ 	if((upKey.isDown || wKey.isDown) && monster.body.velocity.y ==0){
  		monster.body.velocity.y = -500;
  	}
+
 }
